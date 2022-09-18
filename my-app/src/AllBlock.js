62030edf3903bf044e6c50458ece822e24e9d681
Anna Podobrii <anna@nearmeentertainment.com>
2022-09-18 16:28:04 +0300

    added sing

--- my-app/src/AllBlock.js
+++ my-app/src/AllBlock.js
@@ -4,8 +4,19 @@ import MainInfo from "./MainInfo";
 
 export default function AllBlock() {
   return (
-    <div className="AllBlock">
-      <MainInfo city="" />
+    <div>
+      <div className="AllBlock">
+        <MainInfo city="" />
+      </div>
+      <div className="sing">
+        <a
+          href="https://github.com/anna-podobrii/weather-react.git"
+          className="open-code"
+        >
+          Open-source code{` `}
+        </a>
+        by Hanna Podobrii
+      </div>
     </div>
   );
 }
